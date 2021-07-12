93fab0743cf65a798e31a2b5354516c98a247089
Steven Jimenez <stevenjimenez2029@gmail.com>
2021-07-12 15:58:31 -0400

    Add project files.

---
+++ Models/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureQuest.Models
+{
+    public class Player
+    {
+        [Key]
+        int PlayerId { get; set; }
+
+        string Name { get; set; }
+
+        string PlayerType { get; set; }
+
+        int Attack;
+
+        int Defense; 
+
+        int Health;
+
+        int Experience;
+
+        int Level;
+
+        public Player(string aName, string aPlayerType)
+        {
+            this.Name = aName;
+
+            this.PlayerType = aPlayerType;
+            
+
+            if(aPlayerType == "warrior")
+            {
+                this.Attack = 75;
+                this.Defense = 25;
+                this.Health = 50;
+
+            }
+
+            if (aPlayerType == "orc")
+            {
+                this.Attack = 35;
+                this.Defense = 100;
+                this.Health = 50;
+            }
+
+            if (aPlayerType == "mage")
+            {
+                this.Attack = 25;
+                this.Defense = 15;
+                this.Health = 100;
+            }
+            
+
+            this.Experience = 0;
+
+            this.Level = 0;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using AdventureQuest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Adventurer!");
+            Console.WriteLine("Welcome to Adventure Quest.");
+            Console.WriteLine("Lets get playing!");
+            CreatePlayer();
+        }
+
+        static void CreatePlayer()
+        {
+            Console.Write("What is your adventurers name: ");
+            string playerName = Console.ReadLine();
+            Console.WriteLine($"Welcome {playerName}!");
+            Console.WriteLine($"What type of adventure would you like to be?");
+            Console.WriteLine("Warrior - ATK:75 - DF:25 - Hlth:50");
+            Console.WriteLine("Orc - ATK:35 - DF:100 - Hlth:50");
+            Console.WriteLine("Mage - ATK:25 - DF:15 - Hlth:100");
+            Console.Write("Please choice one - 1.Warrior - 2.Orc - 3.Mage: ");
+            int playerChoice = Convert.ToInt32(Console.ReadLine());
+            string playerType;
+            switch (playerChoice)
+            {
+                case 1:
+                    playerType = "warrior";
+                    break;
+                case 2:
+                    playerType = "0rc";
+                    break;
+                case 3:
+                    playerType = "mage";
+                    break;
+                default:
+                    Console.WriteLine("You have made an invalid choice.");
+                    break;
+            }
+            Player newPlayer = new Player(playerName, playerType);
+        }
+    }
+}
