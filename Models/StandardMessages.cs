c1580d6b46c76ffccfc9ab3e2931bba7396bcc12
Steven Jimenez <steven.jimenez2029@gmail.com>
2021-08-02 10:48:44 -0400

    Created standard message class for handling messages to user.

---
+++ Models/StandardMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureQuest.Models
+{
+    class StandardMessages
+    {
+        //This class will handle all stanrdard messages
+    }
+}
